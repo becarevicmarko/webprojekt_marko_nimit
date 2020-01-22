1c92630436ff1505e50eafa96ee2dbdd4b1816ba
becarevicmarko <becarevic420@gmail.com>
2020-01-22 08:36:58 +0100

    Aktualisierung

---
+++ webprojekt_marko_nimit/webprojekt_marko_nimit/Models/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webprojekt_marko_nimit.Models
+{
+    public class Dog
+    {
+        //properties
+        public string Name { get; set; }
+
+        public int Expirience { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string Informations { get; set; }
+
+        public string Description { get; set; }
+
+        //ctors 
+
+        
+
+    }
+}
